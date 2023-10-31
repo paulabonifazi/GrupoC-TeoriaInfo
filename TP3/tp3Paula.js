c821abbe86da050804100caa2662ed134ec3143e
paulabonifazi <paula.bonifazi.aquino@gmail.com>
2023-10-31 10:21:44 -0300

    Update tp3Paula.js

--- TP3/tp3Paula.js
+++ TP3/tp3Paula.js
@@ -2,3 +2,16 @@
 /**FUNCIONES */
 
 /**PROGRAMA PRINCIPAL */
+
+/** ----------------------------------------------------------------------- */
+/**
+ * 1- ordenar la tabla de símbolos según su probabilidad: o sea que primero hay que recorrer todo el archivo, 
+ *    calcular la prob de cada símbolo y luego ordenar la tabla en orden dereciente
+ * 
+ * 2- se selecciona k para que sea mínima
+ * 
+ * 3- se le asigna un bit diferente a cada uno de los subconjuntos en que se divide la fuente
+ * 
+ * 4- repetir 2 y 3 para todos los subconjuntos
+ * 
+ */
